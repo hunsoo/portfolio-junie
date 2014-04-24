fcddbf4e2cc76588f3943d41f32026d35bc78a2a
hunsoo <terlman@gmail.com>
2014-04-24 10:34:59 -0400

    Added carousel caption shadow

--- js/filters.js
+++ js/filters.js
@@ -14,34 +14,16 @@ app.filter('PhotosFilter', function () {
             var item = categoryItems[j];
             if (item.value) {
                 var tag = item.name.toLowerCase();
-                tags.push(tag);
+
                 // if tag string has spaces then strip off spaces and add the resulting string as a tag too
+                // if no space, just add the string as a tag
                 if (tag.indexOf(' ') > -1) {
                     tags.push(tag.replace(/ /g, ''));
+                } else {
+                    tags.push(tag);
                 }
             }
         }
-
-//        for (var i = 0, filterLength = filter.length; i < filterLength; i++) {
-//            var categoryItem = filter[i].data;
-//            var categoryFilter = {
-//                category: categoryItem.category,
-//                tags: []
-//            }
-//            for (var j = 0, categoryLength = categoryItem.length; j < categoryLength; j++) {
-//                var item = categoryItem[j];
-//                if (item.value) {
-//                    var tag = item.name.toLowerCase();
-//                    categoryItem.tags.push(tag);
-//                    // if tag string has spaces then strip off spaces and add the resulting string as a tag too
-//                    if (tag.indexOf(' ') > -1) {
-//                        categoryItem.tags.push(tag.replace(/ /g, ''));
-//                    }
-//                    filterSet.push(categoryFilter);
-//                }
-//            }
-//        }
-
 
         function containTags(photo, index, array) {
             var tags = this;
@@ -58,34 +40,7 @@ app.filter('PhotosFilter', function () {
             return false;
         }
 
-        // now apply each category filter to every photo
-        //var filteredPhotos = [];
-
         var filteredPhotos = photos.filter(containTags, tags);
-//        for (var i = 0, photosLength = photos.length; i < photosLength; i++) {
-//            var photo = photos[i];
-//
-//            // check for different categories of filter
-//            for (var j = 0, filterSetLength = filterSet.length; j < filterSetLength; j++) {
-//                var categoryFilter = filterSet[j];
-//
-//
-//                // if photo tags contain one of category tag
-//                if (photo.tags.indexOf(categoryFilter.tags) > -1) {
-//                }
-//            }
-//
-//            // push if the photo is not already in filteredPhotos
-////            if (filteredPhotos.indexOf(photo) < 0) {
-//            if (photoSelected) {
-//                filteredPhotos.push(photo);
-//            }
-//        }
-
-//        console.log('filteredPhotos - ', filteredPhotos);
-//        for (var i = 0; i < filteredPhotos.length; i++) {
-//            var filteredPhoto = filteredPhotos[i];
-//        }
         return filteredPhotos;
     }
 });
